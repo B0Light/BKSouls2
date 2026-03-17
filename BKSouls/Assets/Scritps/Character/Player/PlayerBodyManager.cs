56d2641f22c8197b1f858ae7bf2265e6a658d85a
B0Light <jangbk1101@gmail.com>
2026-03-18 02:42:32 +0900

    Saddummy Character

--- BKSouls/Assets/Scritps/Character/Player/PlayerBodyManager.cs
+++ BKSouls/Assets/Scritps/Character/Player/PlayerBodyManager.cs
@@ -9,8 +9,10 @@ namespace BK
         PlayerManager player;
         
         [Header("Hair Object")]
-        [SerializeField] public GameObject hair;
         [SerializeField] private GameObject[] hairObjects;
+
+        [Header("Body Object")] 
+        [SerializeField] private GameObject[] bodyObjects;
 
         private void Awake()
         {
@@ -40,36 +42,17 @@ namespace BK
 
         public void ToggleHairType(int hairType)
         {
-            //  DISABLE ALL HAIR
             for (int i = 0; i < hairObjects.Length; i++)
             {
                 hairObjects[i].SetActive(false);
             }
 
-            //  ENABLE CHOOSEN HAIR
             hairObjects[hairType].SetActive(true);
         }
 
         public void SetHairColor()
         {
-            // 1. IF YOU ARE USING A REGULAR MATERIAL AS A HAIR MATERIAL, SIMPLY CHANGE ITS COLOR
-            //  IF YOU ARE USING A MATERIAL WITH MULITPLE COLOR VARIABLES, SIMPLY SET THE CORRECT COLOR
-
-            Color32 hairColor;
-
-            byte red = (byte)player.playerNetworkManager.hairColorRed.Value;
-            byte green = (byte)player.playerNetworkManager.hairColorGreen.Value;
-            byte blue = (byte)player.playerNetworkManager.hairColorBlue.Value;
-
-            hairColor = new Color32(red, green, blue, 255);
-
-            for (int i = 0; i < hairObjects.Length; i++)
-            {
-                SkinnedMeshRenderer skinMeshRenderer = hairObjects[i].GetComponent<SkinnedMeshRenderer>();
-
-                if (skinMeshRenderer != null)
-                    skinMeshRenderer.material.SetColor("_Color_Hair", hairColor);
-            }
+            
         }
     }
 }
